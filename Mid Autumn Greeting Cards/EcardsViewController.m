a03cc114e3b5e8098e1fde5015892b969402e9d8
mobileweb-maker <mymobilesplus@gmail.com>
2020-09-19 11:16:24 +0800

    added share image feature.

--- Mid Autumn Greeting Cards/EcardsViewController.m
+++ Mid Autumn Greeting Cards/EcardsViewController.m
@@ -20,4 +20,20 @@
 }
 
 
+- (IBAction)share:(id)sender {
+    UIButton *btn = (UIButton*)sender;
+    
+    UIImage *image = btn.currentBackgroundImage;
+    NSArray *activityItems = @[image];
+    UIActivityViewController *activityViewControntroller = [[UIActivityViewController alloc] initWithActivityItems:activityItems applicationActivities:nil];
+    activityViewControntroller.excludedActivityTypes = @[];
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
+        activityViewControntroller.popoverPresentationController.sourceView = self.view;
+        activityViewControntroller.popoverPresentationController.sourceRect = CGRectMake(self.view.bounds.size.width/2, self.view.bounds.size.height/4, 0, 0);
+    }
+    [self presentViewController:activityViewControntroller animated:true completion:nil];
+    
+}
+
+
 @end
